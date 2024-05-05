650b23107616d819dba71dd7d455a8c953851fbf
Gagan <gaganv2001@gmail.com>
2024-05-05 11:51:12 +0530

    Introduced Jobs components

---
+++ src/components/Jobs/Jobs.js
@@ -0,0 +1,17 @@
+// Jobs.js
+import React from "react";
+import "./style.css";
+
+const Jobs = () => {
+  return (
+    <div className="jobsContainer">
+      <div className="searchJobs">
+        <p>Search jobs</p>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default Jobs;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import Header from "../../components/Header/Header";
 import Info from "../../components/Info/Info";
+import Jobs from "../../components/Jobs/Jobs";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Info />
+      <Info />   
+      <Jobs /> 
     </div>
   );
 };
