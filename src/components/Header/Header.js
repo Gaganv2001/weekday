23b35e3e1c71f0fb0fc65140557b588ffe859945
Gagan <gaganv2001@gmail.com>
2024-05-05 10:59:30 +0530

    Implemented Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { PiChatTeardropFill } from "react-icons/pi";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="chatContainer">
+        <PiChatTeardropFill color="white" size={28} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Header from "../../components/Header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
